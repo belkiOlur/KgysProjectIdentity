44c658159db82a5ef77e5879cab5508f62de649e
Ibrahim Gun <ibrahim.gun@ogr.iuiedu.tr>
2024-03-08 07:17:13 +0300

    08032024

---
+++ KgysProjectIdentity.Service/Services/IIpPhoneService.cs
@@ -0,0 +1,17 @@
+﻿using KgysProjectIdentity.Core.ViewModels;
+using KgysProjectIdentity.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KgysProjectIdentity.Service.Services
+{
+    public interface IIpPhoneService
+    {
+        bool Add(IpPhoneViewModel phone, string UserName);
+        bool Update(IpPhoneModel phone, string UserName);
+        bool Remove(int id, string UserName);
+    }
+}
---
+++ KgysProjectIdentity.Service/Services/IpPhoneService.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using KgysProjectIdentity.Core.ViewModels;
+using KgysProjectIdentity.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KgysProjectIdentity.Service.Services
+{
+    public class IpPhoneService:IIpPhoneService
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly ILogService _log;
+        private readonly ILogDifferenceDetectService _detect;
+
+        public IpPhoneService(AppDbContext context, IMapper mapper, ILogService log, ILogDifferenceDetectService detect)
+        {
+            _context = context;
+            _mapper = mapper;
+            _log = log;
+            _detect = detect;
+        }
+
+        public bool Add(IpPhoneViewModel phone, string UserName)
+        {
+            try
+            {
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Remove(int id, string UserName)
+        {
+            try
+            {
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(IpPhoneModel phone, string UserName)
+        {
+            try
+            {
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
